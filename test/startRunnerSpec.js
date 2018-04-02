ca4c91aeeea4fa718a5692c76423eb341c240845
Derek Ekins <derek@spathi.com>
2018-04-02 01:01:12 +0100

    added config with defaults and ability to override

--- test/startRunnerSpec.js
+++ test/startRunnerSpec.js
@@ -1,6 +1,6 @@
-// see https://github.com/featurist/indigo-slp/blob/d7419754a23b3a89856623d728ff29369d2f787a/features/support/characters/Associate.js#L34
 const assert = require('assert')
 const assemblies = require('./assemblies')
+
 feature('start runner', assemblies, () => {
   scenario('can start and exit', async assembly => {
     const developer = await assembly.createCharacter('developer', 'Dave')
@@ -12,12 +12,3 @@ feature('start runner', assemblies, () => {
 
   scenario('something else')
 })
-
-//server
- //has actions
- //- run test
- //- debug test
- //watchers
- //- on add
- //- on change
-//
